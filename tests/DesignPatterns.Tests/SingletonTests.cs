dac63017fc4206cf6381b8210c113c7cfb74d6be
kamyab7 <valipourkamyab.official@gmail.com>
2023-06-17 14:59:57 +0330

    Add unit test for singleton pattern

---
+++ tests/DesignPatterns.Tests/SingletonTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Singleton;
+
+namespace DesignPatterns.Tests;
+
+public class SingletonTests
+{
+    [Test]
+    public void BothInstanceShouldBeSame()
+    {
+        // arrange 
+        SingletonSample s1 = SingletonSample.GetInstance();
+        SingletonSample s2 = SingletonSample.GetInstance();
+
+        // act
+
+        // assert
+        s1.Should().BeEquivalentTo(s2);
+    }
+}
