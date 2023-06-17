6eab7959af3a0e9eb62e777d5d4a296d4484bd0a
kamyab <kamyab@techsnovel.co>
2023-06-17 12:44:16 +0330

    Add solution

---
+++ tests/DesignPatterns.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DesignPatterns.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
