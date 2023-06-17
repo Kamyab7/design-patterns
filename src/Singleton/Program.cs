21b6b5dccbb317438c223b0a6dbedb168075e673
kamyab7 <valipourkamyab.official@gmail.com>
2023-06-17 13:33:26 +0330

    Add singleton sample

--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton;
+
+SingletonSample s1 = SingletonSample.GetInstance();
+SingletonSample s2 = SingletonSample.GetInstance();
+
+if (s1 == s2)
+{
+    Console.WriteLine("Singleton works, both variables contain the same instance.");
+}
+else
+{
+    Console.WriteLine("Singleton failed, variables contain different instances.");
+}
---
+++ src/Singleton/SingletonSample.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton;
+
+public sealed class SingletonSample
+{
+    private SingletonSample() { }
+
+    public static SingletonSample Instance = null!;
+
+    public static SingletonSample GetInstance()
+    {
+        if (Instance == null)
+            Instance = new SingletonSample();
+        return Instance;
+    }
+}
